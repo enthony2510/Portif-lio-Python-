975523ae0919bffd3e6e7efa0cd99de31ed8914d
Enthony Passos <enthony.diniz@hotmail.com>
2024-08-06 15:01:36 -0300

    Add files via upload

---
+++ ifandelse.py
@@ -0,0 +1,12 @@
+# Solicitar o salário ao usuário
+salario = float(input("Digite o seu salário: "))
+
+# Verificar se o salário é maior ou igual a 1501 ou menor que 1500
+if salario >= 1501:
+    print("O salário está acima do salário mínimo.")
+else:
+    print("O salário está no salário mínimo.")
+
+
+
+
